eedd7459ffebd706d67370a0897eb6268ec759b7
aidyn1984 <csa.aiden@gmail.com>
2021-03-18 02:30:42 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,11 @@
+# require "pry"
+
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -10,4 +15,18 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
 
+
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
+  end
 end
+
+# genres = ["Thriller", "Science Fiction", "Romance"]
+# genres.each_with_index do |genre, i|
+#     book = Book.new("Book_#{i}")
+#     book.genre = genre
+#     binding.pry
+#   end
+
+# binding.pry
